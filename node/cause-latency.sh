ed32e00d8c722d8e4e04d16ce422d48819cd528f
IkenoXamos <moberlies1337@gmail.com>
2022-12-05 17:18:45 -0500

    Create script to introduce significant latency for a period of 30 minutes

---
+++ node/cause-latency.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script introduces latency to all requests for 30 minutes
+
+# This command identifies the network interfaces we want
+# ip -br l | awk '$1 !~ "lo|vir|wl|eni" { print $1}'
+
+LATENCY=500ms
+ip -br l | awk '$1 !~ "lo|vir|wl|eni" { print $1}' | xargs -l -I % sudo tc qdisc add dev % root netem delay $LATENCY
+
+for i in {1..10}
+do
+  sleep 180
+  echo "Warning: AWS monitoring detected a failing ethernet cable."
+done
+
+ip -br l | awk '$1 !~ "lo|vir|wl|eni" { print $1}' | xargs -l -I % sudo tc qdisc del dev % root netem delay $LATENCY
--- node/consume-cpu.sh
+++ node/consume-cpu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script spikes CPU usage for 10 minutes
+
 consumecpu() {
   dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null |
@@ -9,10 +11,10 @@ consumecpu() {
 
 consumecpu
 
-for i in {1..5}
+for i in {1..10}
 do
-        sleep 60
-        echo "Warning: AWS monitoring taking more resources than expected!"
+  sleep 60
+  echo "Warning: AWS monitoring taking more resources than expected!"
 done
 
 killall dd
